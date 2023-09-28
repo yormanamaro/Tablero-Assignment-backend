2824e3a11c38673ebb5cdbdac5c02c1622a1d385
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-09-28 20:33:34 -0300

    Initial configuration of the path file

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,5 @@
+// Este archivo va a englobar todas nuestras rutas de autenticacion ( login, resgister, perfiles y mas).
+
+import { Router } from "express"; // Nos traemos la funcion de creacion de rutas de express.
+
+const router = Router();
