99d86e3601246f7ac6e5243c58d4cc1fd49dd03b
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-12-07 00:20:14 -0300

    Autentication User

---
+++ src/configEnv.js
@@ -0,0 +1,3 @@
+// Se crea una variable de entorno para el proyecto
+
+export const TOKEN_SECRET = 'some secret key' // para los token secret
---
+++ src/libs/jwt.js
@@ -0,0 +1,18 @@
+// Aqui lo que se va hacer es agregar la funcion que va a recibir los datos y los va a enviar tomados de authCVontroller.js.
+import jwt from 'jsonwebtoken';
+import { TOKEN_SECRET } from '../configEnv.js';
+
+export async function createAccesToken(payload) { // Se ingresa todo el codigo de token dentro de una funcion propia
+  return new Promise((resolve, reject) => { // Todo esto se encierre dentro de una promise para que nos indique si esta bein o mal. en algun momento
+    jwt.sign( // se crea el token
+      payload, // recibe un payload ya no es necesario como antes con un id: userSaved.id, 
+      TOKEN_SECRET, // Se asocia la llave para poder crear un token.
+      {
+        expiresIn: "1d"
+      },
+      (err, token) => {
+        if (err) reject(err) // Si resolvio mal regresa el error
+        resolve(token) // Pero si resolvio bien nos regresa el token.
+      });
+  });
+}
