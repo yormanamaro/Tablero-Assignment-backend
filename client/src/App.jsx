1c15f5121711cf0e8bd720a7b98b3556a75b2cdd
yormanamaro <yormaneduardoamarocarrero@yormans-mbp.lan>
2024-02-28 13:31:07 -0300

    The client file is created for the frontend project

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'; // previamente instalamos libreria React-router-dom como -SE y la importamos
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home page</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/tasks' element={<h1>Tasks Page</h1>} />
+        <Route path='/add-task' element={<h1>new task</h1>} />
+        <Route path='/tasks/:id' element={<h1>update task</h1>} />
+        <Route path='/profile' element={<h1>profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
