fc4e1080c92cee9acc864df04edf3baafcd8f342
yormanamaro <yormaneduardoamarocarrero@yormans-mbp.lan>
2024-03-07 12:59:47 -0300

    provisioning of libraries for react hook forms and folders for union with apis

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,11 +1,21 @@
+import { useForm } from 'react-hook-form'; // importamos la libreria de formularios 
 
 const RegisterPage = () => {
+
+  const { register, handleSubmit } = useForm(); // inicializamos la libreria de formularios de react  con su metodo register
+
   return (
-    <div>
+    <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
 
-      <form action="">
-
-        
+      <form onSubmit={ handleSubmit((values) => {
+        console.log(values);
+        }) }>
+        <input type="text" placeholder='Username' {...register("username", {required: true })} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2' />
+        <input type="email" placeholder='Email' {...register("email", {required: true })} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'  />
+        <input type="password" placeholder='Password' {...register("password", {required: true })} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2' />
+        <button type='submit'>
+          Register
+        </button>
       </form>
 
     </div>
